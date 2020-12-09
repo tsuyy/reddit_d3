cf62d49bcfd0c55f44b66799c73dbf822198607d
yvntsu <yvonnnetsu@gmail.com>
2020-12-09 11:10:56 +0800

    updated scatterplot styles

--- scattor-plot.js
+++ scattor-plot.js
@@ -6,19 +6,20 @@ async function drawScatter() {
   const dateParser = d3.timeParse('%Y-%m-%d');
   const xAccessor = d => dateParser(d['timestamp']);
   const yAccessor = d => +d.calculated_sentiment
+//   const yAccessor = d => +d.upvote_ratio
 //   const colorAccessor = d => +d.upvote_ratio
   const flair = d => d.flair
   const flairColorMap = new Map([["Support", '#0000a4'], 
-                                 ["Discussion", '#d4d4d4'], 
+                                 ["Discussion", '#cac0d9'], 
                                  ["Questions", '#c0adde'], 
                                  ["Firsthand Account", '#512ed7'], 
                                  ["News", '#7951e2'], 
-                                 ["NA", '#cac0d9'],
+                                 ["NA", '#d4d4d4'],
                                  ["Trigger Warning", '#8a63e4'],
                                  ["Resources", '#9a75e4'],
                                  ["Good News", '#a888e3'],
                                  ["Misinformation - debunked", '#b49ae1'],
-                                 ["Missleading and incorrect title", '#391dcd'],
+                                 ["Misleading and incorrect title", '#391dcd'],
                                  ["Deperate mod", '#0a3262'],
                                  ["The answer is NO.", '#1c0abe']]);
 
@@ -26,13 +27,13 @@ async function drawScatter() {
 
   const width = d3.min([
     window.innerWidth * 0.9,
-    window.innerHeight * 0.9,
+    window.innerHeight * 0.8,
   ])
   let dimensions = {
     width: 1200,
     height: width,
     margin: {
-      top: 10,
+      top: 100,
       right: 10,
       bottom: 50,
       left: 50,
@@ -49,8 +50,8 @@ async function drawScatter() {
 
   const wrapper = d3.select("#wrapper")
     .append("svg")
-      .attr("width", dimensions.width)
-      .attr("height", dimensions.height)
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height)
 
   const bounds = wrapper.append("g")
       .style("transform", `translate(${
@@ -74,10 +75,6 @@ async function drawScatter() {
     .range([dimensions.boundedHeight, 0])
     .nice();
 
-//   const colorScale = d3.scaleLinear()
-//     .domain(d3.extent(dataset, colorAccessor))
-//     .range(["skyblue", "darkslategrey"])
-
 const drawDots = (dataset) => {
 
     // 5. Draw data
@@ -92,6 +89,7 @@ const drawDots = (dataset) => {
         .attr("cy", d => yScale(yAccessor(d)))
         .attr("stroke", d => flairColorMap.get(d['flair']))
         .attr("fill", d => flairColorMap.get(d['flair']))
+        .attr("fill-opacity", 0.45)
         .attr("r", 4)
 
     const oldDots = dots.exit()
@@ -111,7 +109,7 @@ const drawDots = (dataset) => {
   const xAxisLabel = xAxis.append("text")
       .attr("class", "x-axis-label")
       .attr("x", dimensions.boundedWidth / 2)
-      .attr("y", dimensions.margin.bottom - 10)
+      .attr("y", dimensions.margin.bottom)
       .html("Month")
 
   const yAxisGenerator = d3.axisLeft()
@@ -127,6 +125,7 @@ const drawDots = (dataset) => {
       .attr("y", -dimensions.margin.left + 10)
       .text("Sentiment")
       .style("text-anchor", "middle")
+
 
 // 7. Set up interactions
 
@@ -174,7 +173,7 @@ const drawDots = (dataset) => {
             .text(datum.body)
     } else {
         tooltip.select("#body")
-            .text("Empty body")
+            .text("")
     }
     
     tooltip.select("#url")
@@ -182,6 +181,7 @@ const drawDots = (dataset) => {
 
     tooltip.select("#flair")
         .text(datum.flair)
+        .style("background-color", d => flairColorMap.get(datum['flair']))
 
     const dateParser = d3.timeParse("%Y-%m-%d")
     const formatDate = d3.timeFormat("%B %-d, %Y")
@@ -205,34 +205,189 @@ const drawDots = (dataset) => {
     d3.selectAll(".tooltipDot")
       .remove()
 
-    tooltip.style("opacity", 1)
+    tooltip.style("opacity", 0)
   }
 }
 drawScatter()
 
-// select the svg area
-var svg = d3.select("#legend")
+// LENGEND
+var legend_svg = d3.select("#legend")
 
-// legend
-svg.append("circle").attr("cx",200).attr("cy",160).attr("r", 6).style("fill", "#0000a4")
-svg.append("text").attr("x", 220).attr("y", 160).text("Support").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("circle").attr("cx",200).attr("cy",190).attr("r", 6).style("fill", "#391dcd")
-svg.append("circle").attr("cx",200).attr("cy",220).attr("r", 6).style("fill", "#512ed7")
-svg.append("text").attr("x", 220).attr("y", 190).text("Missleading and incorrect title").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("text").attr("x", 220).attr("y", 220).text("Firsthand Account").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("circle").attr("cx",200).attr("cy",250).attr("r", 6).style("fill", "#7951e2")
-svg.append("circle").attr("cx",200).attr("cy",280).attr("r", 6).style("fill", "#8a63e4")
-svg.append("text").attr("x", 220).attr("y", 250).text("News").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("text").attr("x", 220).attr("y", 280).text("Trigger Warning").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("circle").attr("cx",200).attr("cy",310).attr("r", 6).style("fill", "#9a75e4")
-svg.append("circle").attr("cx",200).attr("cy",340).attr("r", 6).style("fill", "#a888e3")
-svg.append("text").attr("x", 220).attr("y", 310).text("Resources").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("text").attr("x", 220).attr("y", 340).text("Good News").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("circle").attr("cx",200).attr("cy",370).attr("r", 6).style("fill", "#b49ae1")
-svg.append("circle").attr("cx",200).attr("cy",400).attr("r", 6).style("fill", "#c0adde")
-svg.append("text").attr("x", 220).attr("y", 370).text("Misinformation - debunked").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("text").attr("x", 220).attr("y", 400).text("Questions").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("circle").attr("cx",200).attr("cy",430).attr("r", 6).style("fill", "#cac0d9")
-svg.append("text").attr("x", 220).attr("y", 430).text("NA").style("font-size", "15px").attr("alignment-baseline","middle")
-svg.append("circle").attr("cx",200).attr("cy",460).attr("r", 6).style("fill", "#d4d4d4")
-svg.append("text").attr("x", 220).attr("y", 460).text("Discussion").style("font-size", "15px").attr("alignment-baseline","middle")
+// support
+legend_svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",160).attr("r", 6)
+    .style("fill", "#0000a4")
+
+legend_svg.append("text")
+    .attr("x", 120)
+    .attr("y", 160)
+    .text("Support")
+    .style("font-size", "15px"
+    ).attr("alignment-baseline","middle")
+
+// misleading title
+legend_svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",190).attr("r", 6)
+    .style("fill", "#391dcd")
+
+legend_svg.append("text")
+    .attr("x", 120).attr("y", 190)
+    .text("Misleading and incorrect title")
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+// firsthand
+legend_svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",220)
+    .attr("r", 6)
+    .style("fill", "#512ed7")
+
+legend_svg.append("text")
+    .attr("x", 120)
+    .attr("y", 220)
+    .text("Firsthand Account")
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+// news
+legend_svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",250)
+    .attr("r", 6)
+    .style("fill", "#7951e2")
+
+legend_svg.append("text")
+    .attr("x", 120)
+    .attr("y", 250)
+    .text("News")
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+// trigger warning
+legend_svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",280)
+    .attr("r", 6)
+    .style("fill", "#8a63e4")
+
+legend_svg.append("text")
+    .attr("x", 120)
+    .attr("y", 280)
+    .text("Trigger Warning")
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+// resources
+legend_svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",310)
+    .attr("r", 6)
+    .style("fill", "#9a75e4")
+
+legend_svg.append("text")
+    .attr("x", 120)
+    .attr("y", 310)
+    .text("Resources")
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+// good nes
+legend_svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",340)
+    .attr("r", 6)
+    .style("fill", "#a888e3")
+
+legend_svg.append("text")
+    .attr("x", 120)
+    .attr("y", 340)
+    .text("Good News")
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+// misinformation
+legend_svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",370)
+    .attr("r", 6)
+    .style("fill", "#b49ae1")
+
+legend_svg.append("text")
+    .attr("x", 120)
+    .attr("y", 370)
+    .text("Misinformation - debunked")
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+// quesitons
+legend_svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",400)
+    .attr("r", 6)
+    .style("fill", "#c0adde")
+
+legend_svg.append("text")
+    .attr("x", 120)
+    .attr("y", 400)
+    .text("Questions")
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+// discussion
+legend_svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",430)
+    .attr("r", 6)
+    .style("fill", "#cac0d9")
+
+legend_svg.append("text")
+    .attr("x", 120)
+    .attr("y", 430)
+    .text("Discussion")
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+// NA
+legend_svg.append("circle")
+    .attr("cx",100
+    ).attr("cy",460)
+    .attr("r", 6)
+    .style("fill", "#d4d4d4")
+
+legend_svg.append("text")
+    .attr("x", 120)
+    .attr("y", 460)
+    .text("NA")
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+
+// responsive chart
+function responsivefy(svg) {
+    // get container + svg aspect ratio
+    const wrapper = d3.select(svg.node().parentNode),
+        width = parseInt(svg.style("width")),
+        height = parseInt(svg.style("height")),
+        aspect = width / height;
+
+    // add viewBox and preserveAspectRatio properties,
+    // and call resize so that svg resizes on inital page load
+    svg.attr("viewBox", "0 0 " + width + " " + height)
+        .attr("perserveAspectRatio", "xMinYMin")
+        .call(resize);
+
+    // to register multiple listeners for same event type, 
+    // you need to add namespace, i.e., 'click.foo'
+    // necessary if you call invoke this function for multiple svgs
+    // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+    d3.select(window).on("resize." + container.attr("id"), resize);
+
+    // get width of container and resize svg to fit it
+    function resize() {
+        var targetWidth = parseInt(container.style("width"));
+        svg.attr("width", targetWidth);
+        svg.attr("height", Math.round(targetWidth / aspect));
+    }
+}
